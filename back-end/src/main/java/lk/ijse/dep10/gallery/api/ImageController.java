63fa24befad5f89f0bd9645e4f4f51a5ccde4496
thusithanuwan <nuwwan12403482@gmail.com>
2023-06-13 14:54:41 +0530

    Implemented SaveImage WS

--- back-end/src/main/java/lk/ijse/dep10/gallery/api/ImageController.java
+++ back-end/src/main/java/lk/ijse/dep10/gallery/api/ImageController.java
@@ -1,10 +1,15 @@
 package lk.ijse.dep10.gallery.api;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-import javax.servlet.ServletContext;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.Part;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,5 +34,31 @@ public class ImageController {
             imageFileList.add(url);
         }
         return imageFileList;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<String> saveImages(@RequestPart("images") List<Part> imageFiles,
+                                   UriComponentsBuilder urlBuilder){
+        List<String> imageUrlList = new ArrayList<>();
+
+        if (imageFiles != null){
+            String imageDirPath = servletContext.getRealPath("/images");
+            for (Part imageFile : imageFiles) {
+                String imageFilePath =
+                        new File(imageDirPath, imageFile.getSubmittedFileName()).getAbsolutePath();
+                try {
+                    imageFile.write(imageFilePath);
+                    UriComponentsBuilder cloneBuilder = urlBuilder.cloneBuilder();
+                    String imageUrl = cloneBuilder
+                            .pathSegment("images", imageFile.getSubmittedFileName())
+                            .toUriString();
+                    imageUrlList.add(imageUrl);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return imageUrlList;
     }
 }
